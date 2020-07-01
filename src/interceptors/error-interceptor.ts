8c316181c63901aa15c10144e13e50799b990530
Robson Castilho <estudo@rcastilho.com.br>
2020-07-01 20:55:54 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,10 +3,12 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { BrowserPlatformLocation } from '@angular/platform-browser/src/browser/location/browser_platform_location';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,17 +27,52 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch (errorObj.status) {
+                case 401:
+                    this.handle401();
+                    break;
                 case 403:
                     this.handle403();
                     break;
+
+                default:
+                    this.handleDefaultError(errorObj);
+
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de comunicação',
+            message: 'Email ou Senha incorretos!',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text:'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403(){
         this.storage.setLocaUser(null);
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text:'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
