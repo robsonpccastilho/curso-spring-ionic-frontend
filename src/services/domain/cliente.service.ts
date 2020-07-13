6bdeeb3d693c26d82e70db126887bead945268f8
Robson Castilho <estudo@rcastilho.com.br>
2020-07-13 11:34:40 -0300

    Buscando endereços do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -12,18 +12,8 @@ export class ClienteService{
 
     }
 
-    findByEmail(email: string) : Observable<ClienteDTO> {
-        //Como já está implementado no auth-interceptor não precisa colocar como abaixo
-        /* ----- INICIO --------------------------------------
-        let token = this.storage.getLocaUser().token;
-        let authHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + token }); 
-        return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-            {'headers':authHeader});
-        -------- FIM ----------------------------------------- */
-        //let url = `${API_CONFIG.baseUrl}/clientes/email?value=${email}`
-        //console.log("ClienteService -- url API --> " + url);
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id: String) : Observable<any> {
