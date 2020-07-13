8b23c3a707d5a76413ad722b0d8cc250987d2b83
Robson Castilho <estudo@rcastilho.com.br>
2020-07-13 17:34:33 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,4 +1,4 @@
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, LoadingController } from "ionic-angular";
 import { Component } from "@angular/core";
 import { API_CONFIG } from "../../Config/api.config";
 import { ProdutoService } from "../../services/domain/produto.service";
@@ -18,17 +18,22 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        loader.dismiss();
         this.loadImageUrls();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
   }
 
   loadImageUrls() {
@@ -45,5 +50,13 @@ export class ProdutosPage {
 
   showDetail(produto_id: string ) {
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader; 
   }
 }
