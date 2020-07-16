800fc9dd94e5f2125ee0dd5a0462d97e19db8471
Robson Castilho <estudo@rcastilho.com.br>
2020-07-16 14:52:20 -0300

    talando e testando o apk no dispositivo

--- src/Config/api.config.ts
+++ src/Config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+	baseUrl: "https://curso-spring-ionic-rcastilho.herokuapp.com/",
     bucketBaseUrl: "https://curso-spring-ionic-rpcc.s3-sa-east-1.amazonaws.com"
 }
